4b3a232dc6e164028b15c043514ff8eb43b71668
AdamMatheus <adam.matheus.it@gmail.com>
2021-09-30 21:49:52 +0200

    ok

---
+++ js_2/App.js
@@ -0,0 +1,32 @@
+/*
+var quantity;
+console.log(quantity);
+
+quantity=3;
+console.log(quantity);
+
+var a=5;
+console.log(a);
+
+alert("Merhaba Cohort 9");
+
+var a=10;
+console.log('a baslangic:',a);
+{
+    let b=5;
+    console.log('scope icinde b:',b);
+}
+
+console.log(a);
+console.log(b);
+
+let a=23
+console.log(a);
+
+a=11
+console.log(a)
+*/
+
+// const x= "hello"
+// console.log(x);
+// x='hello world';
---
+++ js_2/App2.js
@@ -0,0 +1,22 @@
+//
+/*var a=5;
+var b=10.03;
+
+console.log(typeof a);
+console.log(typeof b); */
+
+//integerlarda amx 15 digit
+/*var a =123456789012345
+var b=12345667890123451234
+
+console.log(a);
+console.log(b); */
+
+// console.log(Number.Max_SAFE_INTEGER); //9007199254740991
+
+var x = 0.1+0.2;
+
+console.log(x);
+console.log((x.toFixed(2)));
+console.log(typeof c);
+        
