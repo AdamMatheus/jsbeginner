797d018047b99f0760cb4708e35c6d565b3e713a
AdamMatheus <adam.matheus.it@gmail.com>
2021-10-02 15:14:57 +0200

    ok

---
+++ js_3/app.js
@@ -0,0 +1,50 @@
+var a=10;
+var b=2;
+var c=3;
+console.log(b);
+
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a%c);
+//console.log(a**c);
+
+//a=--b
+// a=b--
+// console.log({a});
+// console.log({b});
+//var c=100+50*3
+
+// var d=8;
+// var e=7;
+// var f=2;
+//     console.log({d},{e},{f};
+//     console.log("d+=e",d+=e, {d},{e}));//15
+//     console.log("d-=e",d-=e, {d},{e}));//15
+//     console.log("d*=e",d*=e, {d},{e}));//15
+//     console.log("d/=f",d/=f, {d},{f}));//15
+//     console.log("d%=f",d%=f, {d},{f}));//15
+//     console.log("d&=e",d%=e, {d},{e}));//15
+
+//     g=10/3;
+//     console.log(parseInt(g));
+//     consolelog(Number(g));
+
+//     d=-e
+//     console.log({d},{e});
+
+    // var a=10;
+    // var b="2";
+
+    // c=a+b;
+
+    // console.log(c,typeof c);
+    // d=+c;
+    // console.log(d,typeof d);
+
+
+    //console.log(typeof (a/b),a/b);
+    //console.log(typeof (a*b),a*b);
+    console.log(typeof (a-b),a-b);
+
