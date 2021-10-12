17d8153619e3c8068abbaa5847f7dd8b82772dab
AdamMatheus <adam.matheus.it@gmail.com>
2021-10-12 11:31:37 +0200

    ok

--- js_6/app.js
+++ js_6/app.js
@@ -1,12 +1,15 @@
-var ans1=prompt("Hangi ceviriciyi kullanmak istiyorsun? 1. Celsius>>Fahrenheit , 2.Fahrenheit>Celsius?");
-if (ans1==1 || ans1==2) {
-    var ans2=prompt("sicaklik degeri kac?");
+// var ans1=prompt("Hangi ceviriciyi kullanmak istiyorsun? 1. Celsius>>Fahrenheit , 2.Fahrenheit>Celsius?");
+// if (ans1==1 || ans1==2) {
+//     var ans2=prompt("sicaklik degeri kac?");
 
-    temp=()=>{return(ans1==1 ?
-        alert(ans2*9/5 + 32):
-        alert((ans2-32)*5/9));}             
-    temp(ans2);
-}
-else{
-    alert("lutfen 1 yada 2 seciniz");
-}
+//     temp=()=>{return(ans1==1 ?
+//         alert(ans2*9/5 + 32):
+//         alert((ans2-32)*5/9));}             
+//     temp(ans2);
+// }
+// else{
+//     alert("lutfen 1 yada 2 seciniz");
+// }
+
+
+                
